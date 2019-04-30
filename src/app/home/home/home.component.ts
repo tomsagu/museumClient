c3d06f3229e2236a2cfe9674b5ec466669a591ae
Beatriz <beacorrea@uma.es>
2019-04-30 17:10:13 +0200

    Add calling services in home

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Piece } from 'src/models/Piece';
+import { Brand } from 'src/models/Brand';
+import { PieceProvider } from 'src/providers/PieceProvider';
+import { BrandProvider } from 'src/providers/BrandProvider';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  pieces: Piece[] = [];
+  brands: Brand[] = [];
+  piece1: Piece;
+  piece2: Piece;
+  piece3: Piece;
+
+  constructor(private router: Router,
+    private pieceProvider: PieceProvider,
+    private brandProvider: BrandProvider) { }
 
   ngOnInit() {
+    this.pieceProvider.all().subscribe(pieces => {
+      //this.pieces = [pieces[0],pieces[1],pieces[2]];
+      this.piece1 = pieces[0];
+      this.piece2 = pieces[1];
+      this.piece3 = pieces[2];
+      //console.log(pieces);
+    });
+    this.brandProvider.all().subscribe(brands => {
+      this.brands = brands;
+      // console.log(brands);
+    });
   }
 
 }
