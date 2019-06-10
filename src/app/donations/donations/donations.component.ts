cebaddb11d5c756a75be927ee3cdb7a38c493f08
Tomas <tsayago1997@gmail.com>
2019-06-10 20:43:14 +0200

    add top donations

--- src/app/donations/donations/donations.component.ts
+++ src/app/donations/donations/donations.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PieceProvider } from 'src/providers/PieceProvider';
+import { Piece } from 'src/models/Piece';
 
 @Component({
   selector: 'app-donations',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonationsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pieceProvider: PieceProvider) { }
 
+  pieces: Piece[];
+  donorPieces: Map<String, String[]> = new Map<String, String[]>();
+  donorPiecesArray : String[][];
+
+
+  charge : Boolean = false;
   ngOnInit() {
+    this.pieceProvider.all().subscribe(piece => {
+      this.pieces = piece;
+      this.createMap();
+    });
   }
 
+  createMap() {
+    for (let p of this.pieces) {
+      if (!this.donorPieces.has(p.donor)) {
+
+        this.donorPieces.set(p.donor, [p.donor,p.name]);
+      } else {
+        let aux = this.donorPieces.get(p.donor);
+        aux.push(p.name);
+        this.donorPieces.set(p.donor, aux);
+      }
+    }
+    this.sortMap();
+  }
+
+  sortMap() {  
+    let array = Array.from(this.donorPieces.values());
+    array.sort((a,b)=> b.length - a.length);
+    this.donorPiecesArray = array;
+
+    this.charge = true;
+    }
 }
